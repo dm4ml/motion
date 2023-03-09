a669f754a0c33df6b7802b9cf80196c4ccf4b0ea
Shreya Shankar <ss.shankar505@gmail.com>
2023-03-09 11:38:38 -0800

    Restructure triggers

--- triggers.py
+++ triggers.py
@@ -21,14 +21,6 @@ class SuggestIdea(motion.Trigger):
         # Set up the query suggestion model
         return {"cohere": cohere.Client(os.environ["COHERE_API_KEY"])}
 
-    def shouldFit(self, id, triggered_by):
-        # Check if fit should be called
-        return False
-
-    def fit(self, id, triggered_by):
-        # Fine-tune or fit the query suggestion model
-        pass
-
     def shouldInfer(self, id, triggered_by):
         return True
 
@@ -59,6 +51,14 @@ class SuggestIdea(motion.Trigger):
                 "query", id=new_id, key_values={"text_suggestion": s}
             )
 
+    def shouldFit(self, id, triggered_by):
+        # Check if fit should be called
+        return False
+
+    def fit(self, id, triggered_by):
+        # Fine-tune or fit the query suggestion model
+        pass
+
 
 class Retrieval(motion.Trigger):
     def setUp(self):
@@ -77,6 +77,36 @@ class Retrieval(motion.Trigger):
         if index:
             state.update(index)
         return state
+
+    def inferText(self, id, text):
+        with torch.no_grad():
+            text_inputs = clip.tokenize([text]).to(self.state["device"])
+            text_features = self.state["model"].encode_text(text_inputs)
+
+        # Search the FAISS index for the most similar image
+        text_features = text_features.numpy()
+        scores, indices = self.state["index"].search(
+            text_features / np.linalg.norm(text_features, axis=1),
+            self.state["k"],
+        )
+        for score, index in zip(scores[0], indices[0]):
+            img_id = self.state["index_to_id"][index]
+            new_id = self.store.duplicate("query", id=id)
+            self.store.set(
+                "query",
+                id=new_id,
+                key_values={"img_id": img_id, "img_score": score},
+            )
+
+    def shouldInfer(self, id, triggered_by):
+        # Call infer only on text suggestions
+        if triggered_by.key == "text_suggestion":
+            return True
+
+        return False
+
+    def infer(self, id, triggered_by):
+        self.inferText(id, triggered_by.value)
 
     def shouldFit(self, id, triggered_by):
         # Check if fit should be called
@@ -189,33 +219,3 @@ class Retrieval(motion.Trigger):
         for old_id in ids:
             new_index_to_id[len(new_index_to_id)] = old_id
         return {"index": index, "index_to_id": new_index_to_id}
-
-    def inferText(self, id, text):
-        with torch.no_grad():
-            text_inputs = clip.tokenize([text]).to(self.state["device"])
-            text_features = self.state["model"].encode_text(text_inputs)
-
-        # Search the FAISS index for the most similar image
-        text_features = text_features.numpy()
-        scores, indices = self.state["index"].search(
-            text_features / np.linalg.norm(text_features, axis=1),
-            self.state["k"],
-        )
-        for score, index in zip(scores[0], indices[0]):
-            img_id = self.state["index_to_id"][index]
-            new_id = self.store.duplicate("query", id=id)
-            self.store.set(
-                "query",
-                id=new_id,
-                key_values={"img_id": img_id, "img_score": score},
-            )
-
-    def shouldInfer(self, id, triggered_by):
-        # Call infer only on text suggestions
-        if triggered_by.key == "text_suggestion":
-            return True
-
-        return False
-
-    def infer(self, id, triggered_by):
-        self.inferText(id, triggered_by.value)
