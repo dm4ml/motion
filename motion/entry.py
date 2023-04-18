46231a8b8a309ebc798b1d63244b820f1a44e800
Shreya Shankar <ss.shankar505@gmail.com>
2023-04-17 18:07:59 -0700

    Remove projects for now

--- motion/entry.py
+++ motion/entry.py
@@ -84,16 +84,14 @@ def create_app(name: str, author: str) -> None:
     if len(name.split(" ")) > 1:
         raise ValueError("Name cannot contain spaces.")
 
-    project_path = os.path.join('projects', name)
-    if os.path.exists(project_path):
+    if os.path.exists(name):
         raise ValueError(f"Directory {name} already exists.")
 
     # Copy over the example project
     shutil.copytree(os.path.join(os.path.dirname(__file__), "examples/basic"), name)
 
     # Create store setup file
-    os.makedirs(project_path)
-    with open(os.path.join(project_path, "mconfig.py"), "w") as f:
+    with open(os.path.join(name, "mconfig.py"), "w") as f:
         f.write(
             open(
                 os.path.join(
