b28f030d8e4eb016a8a81f296cc2e622f9ec4964
Shreya Shankar <ss.shankar505@gmail.com>
2023-10-30 15:27:21 -0700

    Fixing mypy

--- motion/copy_utils.py
+++ motion/copy_utils.py
@@ -27,18 +27,18 @@ async def copy_motion_db(src: RedisParams, dest: RedisParams) -> None:
 
     # establish connections
 
-    src_con = redis.Redis(
+    src_con: redis.Redis = redis.Redis(
         host=src.host, port=src.port, password=src.password, db=src.db
     )
 
-    if await src.ping() is False:
+    if await src_con.ping() is False:
         raise ValueError("Could not connect to source Redis instance.")
 
-    dest_con = redis.Redis(
+    dest_con: redis.Redis = redis.Redis(
         host=dest.host, port=dest.port, password=dest.password, db=dest.db
     )
 
-    if await dest.ping() is False:
+    if await dest_con.ping() is False:
         raise ValueError("Could not connect to destination Redis instance.")
 
     # Copy all keys prefixed MOTION_STATE: and MOTION_VERSION:
@@ -47,7 +47,7 @@ async def copy_motion_db(src: RedisParams, dest: RedisParams) -> None:
         for key_prefix in key_prefixes:
             logger.info(f"Copying keys with prefix {key_prefix}")
 
-            cursor = b"0"
+            cursor = 0
             while cursor:
                 cursor, keys = await src_con.scan(cursor=cursor, match=f"{key_prefix}*")
                 if not keys:
@@ -66,6 +66,9 @@ async def copy_motion_db(src: RedisParams, dest: RedisParams) -> None:
                 await pipeline.execute()
 
                 logger.info(f"Copied {len(keys)} keys with prefix {key_prefix}")
+
+                # Make sure to convert cursor back to an integer if it's not already
+                cursor = int(cursor) if cursor is not None else None
 
             logger.info(f"Finished copying keys with prefix {key_prefix}.")
 
