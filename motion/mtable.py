8673b591d4544203a3694e700b530ccdb2e5f94a
Shreya Shankar <ss.shankar505@gmail.com>
2023-11-29 11:03:16 -0800

    Adding MTable data structure.

--- motion/mtable.py
+++ motion/mtable.py
@@ -5,7 +5,10 @@ state.
 
 It is a wrapper around a pyarrow table.
 """
+<<<<<<< HEAD
 import os
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
 import secrets
 from typing import Any, List, Optional, Union
 
@@ -17,6 +20,7 @@ import pyarrow.parquet as pq
 
 
 class MTable:
+<<<<<<< HEAD
     """
     A class representing a table in a motion component instance state,
     acting as a wrapper around a PyArrow table. It provides functionalities
@@ -67,6 +71,15 @@ class MTable:
             NotImplementedError: If external is True, indicating the table
                 should be created through class methods.
         """
+=======
+    def __init__(
+        self,
+        data: pa.Table,
+        filesystem: Optional[Any] = None,
+        identifier: Optional[str] = None,
+        external: bool = True,
+    ) -> None:
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         if external:
             # Raise error asking to use the class methods for creating tables
             raise NotImplementedError("Use the class methods for creating tables")
@@ -74,6 +87,7 @@ class MTable:
         self._filesystem = filesystem
         self._identifier = identifier
         self._data = data
+<<<<<<< HEAD
         self._prefix = os.path.expanduser("~/.motion")
 
         # Make the prefix directory if it doesn't exist
@@ -83,10 +97,16 @@ class MTable:
     @property
     def data(self) -> pa.Table:
         """Gets the PyArrow table data. You can modify this object directly."""
+=======
+
+    @property
+    def data(self) -> pa.Table:
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         return self._data
 
     @data.setter
     def data(self, data: pa.Table) -> None:
+<<<<<<< HEAD
         """Sets the PyArrow table data."""
         self._data = data
 
@@ -99,20 +119,37 @@ class MTable:
     @filesystem.setter
     def filesystem(self, filesystem: pa.fs.FileSystem) -> None:
         """Sets the filesystem object used for reading/writing data."""
+=======
+        self._data = data
+
+    @property
+    def filesystem(self) -> Optional[Any]:
+        return self._filesystem
+
+    @filesystem.setter
+    def filesystem(self, filesystem: Any) -> None:
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         self._filesystem = filesystem
 
     @property
     def identifier(self) -> Optional[str]:
+<<<<<<< HEAD
         """Gets the unique identifier for the table."""
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         return self._identifier
 
     @identifier.setter
     def identifier(self, identifier: str) -> None:
+<<<<<<< HEAD
         """Sets the unique identifier for the table."""
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         self._identifier = identifier
 
     @classmethod
     def from_pandas(cls, df: pd.DataFrame) -> "MTable":
+<<<<<<< HEAD
         """
         Creates an MTable instance from a pandas DataFrame.
         This may not be zero copy.
@@ -123,11 +160,14 @@ class MTable:
         Returns:
             MTable: An MTable instance representing the DataFrame.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         table = pa.Table.from_pandas(df)
         return cls(table, external=False)
 
     @classmethod
     def from_arrow(cls, data: pa.Table) -> "MTable":
+<<<<<<< HEAD
         """
         Creates an MTable instance from a PyArrow Table. This is zero copy.
 
@@ -137,10 +177,13 @@ class MTable:
         Returns:
             MTable: An MTable instance representing the Table.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         return cls(data, external=False)
 
     @classmethod
     def from_schema(cls, schema: pa.Schema) -> "MTable":
+<<<<<<< HEAD
         """Creates an MTable instance from a PyArrow Schema.
         This will create an empty table with the specified schema.
 
@@ -150,6 +193,9 @@ class MTable:
         Returns:
             MTable: An MTable instance
         """
+=======
+        # Creates an empty table from a schema
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         table = pa.Table.from_arrays(
             [pa.array([], type=field.type) for field in schema], schema=schema
         )
@@ -162,8 +208,12 @@ class MTable:
             identifier = self.identifier
             if not identifier:
                 # Create a random identifier phrase
+<<<<<<< HEAD
                 identifier = "mtable_" + str(secrets.token_hex(12))
                 identifier = self._prefix + "/" + identifier
+=======
+                identifier = str(secrets.token_hex(12))
+>>>>>>> ce9a6ef (Adding MTable data structure.)
 
             identifier = identifier + ".parquet"
             pq.write_table(self.data, identifier, filesystem=self.filesystem)
@@ -201,6 +251,7 @@ class MTable:
 
     # Methods to add rows and columns
     def add_row(self, row: dict) -> None:
+<<<<<<< HEAD
         """
         Adds a new row to the table.
 
@@ -213,6 +264,8 @@ class MTable:
             TypeError: If there is a data type mismatch or conversion error.
             Exception: For other generic exceptions.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         try:
             # Create a dictionary with the same schema structure
             # but with the new row's data
@@ -234,6 +287,7 @@ class MTable:
             raise e
 
     def remove_row(self, i: int) -> pa.Table:
+<<<<<<< HEAD
         """
         Removes a row from the table at the specified index.
 
@@ -243,6 +297,8 @@ class MTable:
         Raises:
             IndexError: If the specified index is out of bounds.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         if i < 0 or i >= self.data.num_rows:
             raise IndexError("Row index out of bounds")
 
@@ -254,6 +310,7 @@ class MTable:
         return pa.concat_tables([part1, part2])
 
     def add_column(self, i: int, field_: Union[str, pa.Field], column: Any) -> None:
+<<<<<<< HEAD
         """
         Adds a new column to the table at the specified index.
 
@@ -266,6 +323,8 @@ class MTable:
         Raises:
             ValueError: If a column with the same name already exists in the table.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         # Check if the column already exists
         if field_ in self.data.schema.names:
             raise ValueError(f"Column '{field_}' already exists.")
@@ -274,6 +333,7 @@ class MTable:
         self.data = self.data.add_column(i, field_, column)
 
     def append_column(self, field_: Union[str, pa.Field], column: Any) -> None:
+<<<<<<< HEAD
         """
         Appends a new column to the end of the table.
 
@@ -285,6 +345,8 @@ class MTable:
         Raises:
             ValueError: If a column with the same name already exists in the table.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         # Check if the column already exists
         if field_ in self.data.schema.names:
             raise ValueError(f"Column '{field_}' already exists.")
@@ -293,22 +355,28 @@ class MTable:
         self.data = self.data.append_column(field_, column)
 
     def remove_column(self, i: int) -> None:
+<<<<<<< HEAD
         """
         Removes a column from the table at the specified index.
 
         Args:
             i (int): The index of the column to be removed.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         # Remove the column
         self.data = self.data.remove_column(i)
 
     def remove_column_by_name(self, name: str) -> None:
+<<<<<<< HEAD
         """
         Removes a column from the table by its name.
 
         Args:
             name (str): The name of the column to be removed.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         # Remove the column
         self.data = self.data.remove_column(self.data.schema.get_field_index(name))
 
@@ -321,6 +389,7 @@ class MTable:
         metric: str,
         resulting_columns: Optional[List[str]] = None,
     ) -> pa.Table:
+<<<<<<< HEAD
         """
         Performs a k-nearest neighbors search on a vector column in the table.
 
@@ -339,6 +408,8 @@ class MTable:
                 in order, and their distances.
         """
 
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         indices, distances = fvs.search_arrow(
             self.data, vector_column_name, query_point, k, metric
         )
@@ -368,6 +439,7 @@ class MTable:
         query_point: Union[list, "np.ndarray"],
         metric: str,
     ) -> pa.Table:
+<<<<<<< HEAD
         """
         Applies a distance metric to each row in a specified vector column of
         the table.
@@ -384,6 +456,8 @@ class MTable:
             pa.Table: A new PyArrow Table with the distance calculation
                 appended as a new column.
         """
+=======
+>>>>>>> ce9a6ef (Adding MTable data structure.)
         distances: pa.Array = fvs.apply_distance_arrow(
             self.data, vector_column_name, query_point, metric
         )
